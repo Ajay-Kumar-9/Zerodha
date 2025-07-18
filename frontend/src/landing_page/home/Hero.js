d389ca8f0f6d5f9de90d8e272172e4dfb49487f4
Ajay-Kumar-9 <161349732+Ajay-Kumar-9@users.noreply.github.com>
2025-07-18 20:54:43 +0530

    changes in Homepage

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
+    const navigate = useNavigate(); // Hook for navigation
+
+    const handleSignup = () => {
+        navigate('/signup'); // Navigate to signup page
+    }
+
     return (
        <div className='container p-5'>
         <div className='row text-center'>
-        <img src='media/images/homeHero.png' alt='Hero Img' className='mb-5'/>
-        <h1 className='mt-5'> Investment in everything</h1>
-        <p>Online platform to invest in stocks, derivatives, mutual funds and more</p>
-        <button className='p-2 btn btn-primary fs-5' style={{width:"20%", margin:" 0 auto"}}>Signup Now</button>
+            <img src='media/images/homeHero.png' alt='Hero Img' className='mb-5'/>
+            <h1 className='mt-5'> Investment in everything</h1>
+            <p>Online platform to invest in stocks, derivatives, mutual funds and more</p>
+            <button 
+                className='p-2 btn btn-primary fs-5' 
+                style={{width:"20%", margin:" 0 auto"}}
+                onClick={handleSignup} // Add click handler
+            >
+                Signup Now
+            </button>
         </div>
        </div>
-      );
+    );
 }
 
 export default Hero;
